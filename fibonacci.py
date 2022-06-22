8df77a432b566e994a982d38bbdc6e8cc306a2a6
Chagas011 <washington.chagas.9@hotmail.com>
2022-06-22 19:51:30 -0300

    Sequencia de Fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,23 @@
+
+valor = int(input('informe um numero para a sequencia de Fibonacci: '))
+
+
+def sequencia_fibonacci(valor):
+    anterior = 0
+    proxima = 1
+    soma = 1
+    lista = []
+    for _ in range(0, valor + 1):
+        lista.append(anterior)
+        soma = anterior + proxima
+        anterior = proxima
+        proxima = soma
+
+    if valor in lista:
+        print(f'O numero informado: [{valor}] esta na lista {lista}')
+
+    else:
+        print(f'O numero informado: [{valor}] nao esta na lista {lista}')
+
+
+sequencia_fibonacci(valor)
